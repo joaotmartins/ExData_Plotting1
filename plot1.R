d4ecd85beb92b39c8acfa986b79e8750d792b0b4
joaotmartins <joao.t.martins@gmail.com>
2015-12-11 16:24:41 +0000

    Created R script and file for plot 1.

---
+++ plot1.R
@@ -0,0 +1,23 @@
+## Reads the "Individual household electric power consumption Data Set" from the
+## UC Irvine ML repository.
+
+library(data.table)
+library(dplyr)
+
+fd <- fread(input = "household_power_consumption.txt", na.strings = "?")
+
+# subset data immediatly
+
+fd.sub <- filter(fd, Date == "1/2/2007" | Date == "2/2/2007")
+
+# open PNG device
+png(filename = "plot1.png", width = 480, height = 480)
+
+# Draw histogram of global active power
+hist(fd.sub$Global_active_power, 
+     xlab = "Global Active Power (kilowatts)",
+     main = "Global Active Power",
+     col = c("red"))
+
+# shut down PNG device, saves file
+dev.off()
