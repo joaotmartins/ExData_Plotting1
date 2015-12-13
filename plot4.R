b015c71a70a56cb06dbdaaf1288b1f600df12a99
joaotmartins <joao.t.martins@gmail.com>
2015-12-13 19:46:26 +0000

    Corrected comments on plot3, added plot 4.

--- plot4.R
+++ plot4.R
@@ -1,5 +1,5 @@
 ## Reads the "Individual household electric power consumption Data Set" from the
-## UC Irvine ML repository, and plots energy sub metering over a time period.
+## UC Irvine ML repository, and plots 4 different data series in a grid.
 
 library(data.table)
 library(dplyr)
@@ -15,7 +15,7 @@ fd.sub <- mutate(fd.sub,
                  datetime = as.POSIXct(strptime(paste(Date, " ", Time), 
                                                 "%d/%m/%Y    %H:%M:%S")))
 # open PNG device
-png(filename = "plot3.png", width = 480, height = 480)
+png(filename = "plot4.png", width = 480, height = 480)
 
 # set locale to english for correct day names
 cloc <- Sys.getlocale("LC_TIME")
@@ -23,7 +23,22 @@ cloc <- Sys.getlocale("LC_TIME")
 # set locale on windows systems
 Sys.setlocale("LC_TIME", "English_US")
 
-# draw graph of energy sub metering
+# set 2x2 plotting grid
+par(mfrow = c(2,2))
+
+# plot 1 - global active power
+plot(fd.sub$datetime, fd.sub$Global_active_power, 
+     type = "l",
+     ylab = "Global Active Power (kilowatts)",
+     xlab = "")
+
+# plot 2 - voltage
+plot(fd.sub$datetime, fd.sub$Voltage, 
+     type = "l",
+     ylab = "Voltage",
+     xlab = "datetime")
+
+# plot 3 - sub metering
 plot(fd.sub$datetime, fd.sub$Sub_metering_1, 
      type = "l",
      col = "black",
@@ -38,7 +53,13 @@ lines(fd.sub$datetime, fd.sub$Sub_metering_3,
 legend(x = "topright", 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
        col = c("black", "red", "blue"),
-       lty = 1, lwd = 1)
+       lty = 1, lwd = 1, bty = "n")
+
+# plot 4 - global reactive power
+plot(fd.sub$datetime, fd.sub$Global_reactive_power, 
+     type = "l",
+     ylab = "Global_reactive_power",
+     xlab = "datetime")
 
 Sys.setlocale("LC_TIME", cloc)
 
